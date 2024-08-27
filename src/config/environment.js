d7adf80e4eabdb9f887cd2d46e83453c15475a03
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2024-08-27 11:46:22 +0530

    feat: db config added

---
+++ src/config/environment.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+module.exports = {
+  PORT: process.env.PORT || 5000,
+  MONGO_URI: process.env.MONGO_URI || '',
+  JWT_SECRET: process.env.JWT_SECRET || 'your_secret_key',
+  EMAIL_HOST: process.env.EMAIL_HOST || '',
+  EMAIL_PORT: process.env.EMAIL_PORT || '',
+  EMAIL_USER: process.env.EMAIL_USER || '',
+  EMAIL_PASS: process.env.EMAIL_PASS || '',
+};
