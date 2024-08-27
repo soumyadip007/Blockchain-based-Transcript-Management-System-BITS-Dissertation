4ba65eac175ced40db661cd7f59effcf96d06a52
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2024-08-27 11:46:14 +0530

    feat: auth added

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../config/environment');
+
+const authenticateToken = (req, res, next) => {
+  const token = req.headers['authorization'];
+
+  if (!token) return res.status(401).json({ message: 'No token provided' });
+
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) return res.status(403).json({ message: 'Invalid token' });
+
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = authenticateToken;
